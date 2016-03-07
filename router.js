2405fb2fb8c3619ef237b0e91af6cd7fdb82210d
rodrigoramos94 <rramosparaiso@gmail.com>
2016-03-07 02:01:06 +0100

    Test

---
+++ router.js
@@ -0,0 +1,14 @@
+function route(handle, pathname, response, postData) {
+  console.log("A punto de rutear una peticion para " + pathname);
+
+  if(typeof handle[pathname] === 'function'){
+  	handle[pathname](response, postData);
+  }else{
+  	console.log("No hay manipulador de peticion para " + pathname);
+  	response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("404 No Encontrado");
+    response.end();
+  }
+}
+
+exports.route = route;
