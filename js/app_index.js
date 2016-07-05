31a43f63a61fafaeadc36a0aa4c58abc90c247d6
rodrigoramos94 <rramosparaiso@gmail.com>
2016-07-05 16:37:01 +0200

    2 botones añadidos
    Añadidos 2 metodos de ocultar y mostrar
    
    añadidos 2 botones
    
    aumentado el refresh a 20 sec

--- js/app_index.js
+++ js/app_index.js
@@ -1,4 +1,12 @@
 
+window.addEventListener("load",function() {
+  
+	hideAddressBar();
+});
+
+
+window.addEventListener("orientationchange", hideAddressBar);
+
 function hideAddressBar(){
 
 	setTimeout(function(){
@@ -9,10 +17,12 @@ function hideAddressBar(){
 
 }
 
-window.addEventListener("load",function() {
-  
-	hideAddressBar();
-});
+function hideAddressBar(){
 
+	setTimeout(function(){
+		document.body.style.height = document.body.style.height + 1;
+		window.scrollTo(0, 1);
+		document.body.style.height = window.outerHeight;
+	}, 50);
 
-window.addEventListener("orientationchange", hideAddressBar);
+}
