e16c7c06e5b8b98970457298915aab85abbdca9c
rodrigoramos94 <rramosparaiso@gmail.com>
2016-07-05 17:12:28 +0200

    h

--- js/app_index.js
+++ js/app_index.js
@@ -1,13 +1,14 @@
 
 window.addEventListener("load",function() {
-  
+  bar_size = document.body.style.height;
 });
 
-
+var bar_size;
 
 function hideAddressBar(){
 
 	setTimeout(function(){
+		
 		document.body.style.height = screen.height + 1;
 		window.scrollBy(0, 1);
 		//document.body.style.height = screen.height;
@@ -18,7 +19,7 @@ function hideAddressBar(){
 function showAddressBar(){
 
 	setTimeout(function(){
-		document.body.style.height = screen.height;
+		document.body.style.height = bar_size;
 		window.scrollBy(0, -100);
 	}, 50);
 
