b225e6e9d50a8172442efaa9d03a929be3500f76
rodrigoramos94 <rramosparaiso@gmail.com>
2016-07-05 15:49:05 +0200

    d

--- js/app_index.js
+++ js/app_index.js
@@ -1,22 +1,20 @@
 
 function hideAddressBar(){
-if(!window.location.hash)
-  {
-      if(document.height < window.outerHeight + 10)
-      {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
-      }
 
-      setTimeout(function()
-      {
-      	window.scrollTo(0, 1);
-      }, 50);
-  }
+	if(document.height < window.outerHeight + 10){
+		document.body.style.height = (window.outerHeight + 50) + 'px';
+	}
+
+	setTimeout(function(){
+		window.scrollTo(0, 1);
+	}, 50);
+
 }
 
 window.addEventListener("load",function() {
   setTimeout(function(){
 	window.scrollTo(0, 1);
+	  document.body.style.height = window.outerHeight;
   }, 0);
 	
 	
