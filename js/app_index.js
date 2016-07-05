6aad35290447416f7be81f461c1b10dc04b28f9d
rodrigoramos94 <rramosparaiso@gmail.com>
2016-07-05 16:52:44 +0200

    ScrollBY

--- js/app_index.js
+++ js/app_index.js
@@ -10,8 +10,8 @@ window.addEventListener("orientationchange", hideAddressBar);
 function hideAddressBar(){
 
 	setTimeout(function(){
-		document.body.style.height = document.body.style.height + 1;
-		window.scrollTo(0, 1);
+		document.body.style.height = screen.height + 1;
+		window.scrollBy(1, 0);
 		document.body.style.height = screen.height;
 	}, 50);
 
@@ -20,7 +20,7 @@ function hideAddressBar(){
 function showAddressBar(){
 
 	setTimeout(function(){
-		window.scrollTo(0, -1);
+		window.scrollBy(100, 0);
 		document.body.style.height = screen.height;
 	}, 50);
 
