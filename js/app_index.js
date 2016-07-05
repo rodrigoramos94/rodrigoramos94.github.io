0ce2e33c9dc40db0b1b2d516b731aa659c09f358
rodrigoramos94 <rramosparaiso@gmail.com>
2016-07-05 11:56:45 +0200

    Ventana emergente añadida

--- js/app_index.js
+++ js/app_index.js
@@ -1,3 +1,11 @@
+
 function eraseHeadBrowser(){
-	
+	open('index.html', 'principal', 'location=no,menubar=no,status=no,toolbar=no'); 
+    cerrar();
 }
+
+function cerrar() { 
+	var ventana = window.self; 
+	ventana.opener = window.self; 
+	ventana.close(); 
+} 
